280995487cc490b0ec9cfc87f4a5f91b11db8854
stefanledin <stefanledin@gmail.com>
2015-02-10 10:02:25 +0100

    $assetsDir

--- includes/enqueue-scripts-styles.php
+++ includes/enqueue-scripts-styles.php
@@ -3,10 +3,10 @@ function enqueue_scripts_and_styles () {
 	$themeDir = get_template_directory_uri();
 	$assetsDir = $themeDir . '/assets';
 	$assetsPath = get_stylesheet_directory() . '/assets';
-	
+
 	// wp_enqueue_style( $handle, $src, $deps, $ver, $media );
-	wp_enqueue_style( 'stylesheet', get_template_directory_uri().'/assets/css/style.css', null, filemtime($assetsPath.'/css/style.css'), null );
-	
+	wp_enqueue_style( 'stylesheet', $assetsDir.'/css/style.css', null, filemtime($assetsPath.'/css/style.css'), null );
+
 	// wp_enqueue_scripts($handle, $src, $deps, $ver, $in_footer);
 	wp_deregister_script( 'jquery' );
 	wp_register_script( 'jquery', '//code.jquery.com/jquery-latest.min.js', null, null, true );
