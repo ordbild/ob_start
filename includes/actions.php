5cc29dbd0335242726e83d1bf34a0db187507916
John <hej@johnsvensson.com>
2016-08-15 14:08:29 +0200

    added theme support for auto title tag

--- includes/actions.php
+++ includes/actions.php
@@ -4,3 +4,18 @@
  */
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ */
+function ob_start_setup() {
+  /*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+}
+add_action( 'after_setup_theme', 'ob_start_setup' );
--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title><?php wp_title(''); ?></title>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class();?>>
