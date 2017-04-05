3c76dde1601f43356694a7b9b2d2a14493def8b8
John <hej@johnsvensson.com>
2017-04-05 12:36:28 +0200

    changed register_nav_menu names

--- includes/menus.php
+++ includes/menus.php
@@ -1,8 +1,8 @@
 <?php
-register_nav_menus(
-    array(
-        'secondary' => 'Sekundärmeny',
-        'main_menu' => 'Huvudmeny',
-    )
-);
-
+register_nav_menus( array(
+	'primary'   => __( 'Primary Menu', 'obstart' ),
+	'secondary' => __( 'Secondary Menu', 'obstart' ),
+	'utility'   => __( 'Utility Menu', 'obstart' ),
+	'footer'    => __( 'Footer Menu', 'obstart' ),
+	'mobile'    => __( 'Mobile Menu', 'obstart' ),
+) );
