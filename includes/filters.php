7137ec019115fd4c9ce826a07c6031da9bdf96e7
Stefan Ledin <stefan.ledin@gmail.com>
2018-09-14 15:36:10 +0200

    Embed runt YouTube

--- includes/filters.php
+++ includes/filters.php
@@ -1,4 +1,14 @@
 <?php
+
+/**
+ * Wrappar YouTube-embeds med div.embed.
+ */
+function wrap_embed_with_div($html, $url, $attr)
+{
+    return '<div class="embed">' . $html . '</div>';
+}
+add_filter('embed_oembed_html', 'wrap_embed_with_div', 10, 3);
+
 /**
  * Lägg till extra classer på body
  */
