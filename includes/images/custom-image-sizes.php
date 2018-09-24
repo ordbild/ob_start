9011793e6a838db67468fdc895a9644a9cb402c9
Stefan Ledin <stefan.ledin@gmail.com>
2018-09-24 10:55:55 +0200

    En rimlig boilerplate med nya bildstorlekar

--- includes/images/custom-image-sizes.php
+++ includes/images/custom-image-sizes.php
@@ -1,8 +1,18 @@
 <?php
-// Tablet portrait
-add_image_size( 'tablet-portrait', '880', '980', false );
-// iPhone landscape
-add_image_size( 'iphone-landscape', '480', '580', false );
-
-// Boilerplate
-// add_image_size( $name, $width, $height, $crop );
+/**
+ * Breakpoints
+ * ===
+ * 320 (Thumbnail)
+ * 480
+ * 560 
+ * 638
+ * 730 (Medium)
+ * 1110 (large)
+ * 1440
+ */
+add_image_size('width_480', 480, null);
+add_image_size('width_560', 560, null);
+add_image_size('width_638', 638, null);
+add_image_size('width_730', 730, null);
+add_image_size('width_1440', 1440, null);
+add_image_size('width_2880', 2880, null);
